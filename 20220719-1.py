1ab278801faaf5a3dc11f9907391912607a978b1
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-07-19 12:43:27 +0800

    Update 20220719-1.py
    Using Python "Itertools" module "Combinations" method

--- 20220719-1.py
+++ 20220719-1.py
@@ -4,5 +4,4 @@ inp = [3, 4, 6, 5, 7, 6, 2, 4, 9, 8, 2]
 seq = set(list(combinations(inp, 3)))
 eq = (lambda x: x[0] + x[1] + x[2] == 15)
 results = list(filter(eq, seq))
-for result in results:
-    print(list(result))
+[list(result) for result in results]
