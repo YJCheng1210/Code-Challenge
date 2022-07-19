2910a2c04a2a9df26940cc8769ff1be5249a2760
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-07-19 12:39:18 +0800

    Add files via upload

---
+++ 20220719-1.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+inp = [3, 4, 6, 5, 7, 6, 2, 4, 9, 8, 2]
+seq = set(list(combinations(inp, 3)))
+eq = (lambda x: x[0] + x[1] + x[2] == 15)
+results = list(filter(eq, seq))
+for result in results:
+    print(list(result))
