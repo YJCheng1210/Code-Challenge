31f50a1e23c7c4088e94598c11631e5844255fac
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-07-21 12:35:54 +0800

    Add files via upload

---
+++ 20220416.py
@@ -0,0 +1,9 @@
+eggs =['r2', 'g1', 'y2', 'r3', 'g3', 'r1', 'y1', 'g2', 'y3']
+
+eggs.sort(key = lambda s: s[0])
+
+eggs.sort(key = lambda s: s[1])
+
+for i in range(0, len(eggs), 3):
+    basket = [eggs[i], eggs[i+1], eggs[i+2]]
+    print(basket, end=' ')
