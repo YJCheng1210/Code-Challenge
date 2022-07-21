f344f9d4e7aabf526cfd72d775f5f370d6993348
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-07-21 21:24:59 +0800

    Add files via upload

---
+++ 20220127.py
@@ -0,0 +1,11 @@
+# sort expressions - list by expression-value ('4 + 2' = 6)
+
+expressions = [5, '4+3', '3', '2*2', '2**3', '12/6', 1, '9-4']
+
+indexes = ['0', '1', '2', '3', '4', '5', '6', '7']
+
+expressions = [[v, v] if isinstance(v,int) else [v, eval(v)] for v in expressions]
+
+expressions = sorted(expressions, key=lambda e:e[1])
+
+print([e[0] for e in expressions])
