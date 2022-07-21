3c78430a173486a411670ad6ac934af15649d4e7
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-07-21 14:50:33 +0800

    Add files via upload

---
+++ 20220721.py
@@ -0,0 +1,16 @@
+# Print the letter frequency of the string. Spaces don't count
+# upper/lower-case are same M==m
+
+input = 'Nobody expects the spanish inquisition'
+input = input.lower()
+
+stat = {}
+
+# Interate every line.
+for i in range(0, len(input)):
+    if input[i] not in stat:
+        stat[input[i]] = 0
+    stat[input[i]] += 1
+stat.pop(' ')
+stat = sorted(stat.items(), key = lambda e:e[1], reverse=True)        # Sort by number of occurrences, in reverse order
+print(stat)
